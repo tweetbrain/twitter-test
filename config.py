395b1d0559cbbb5a96e19605fdfbb83bafcf59f0
Nathaniel Louis Tisuela <natetisuela@gmail.com>
2020-10-20 00:01:44 -0700

    basic functionality

---
+++ config.py
@@ -0,0 +1,10 @@
+import json
+
+with open('secrets.json', 'r') as secrets_file:
+    secrets = json.load(secrets_file)
+
+class Config:
+    CONSUMER_KEY = secrets["CONSUMER_KEY"]
+    CONSUMER_SECRET = secrets["CONSUMER_SECRET"]
+    ACCESS_TOKEN = secrets["ACCESS_TOKEN"]
+    ACCESS_SECRET = secrets["ACCESS_SECRET"]
--- twitter-test.py
+++ twitter-test.py
@@ -1,9 +1,21 @@
+import tweepy
+from config import Config
+auth = tweepy.AppAuthHandler(Config.CONSUMER_KEY, Config.CONSUMER_SECRET)
 
+
+
+#Construct the API instance
+api = tweepy.API(auth, wait_on_rate_limit=True) # create an API object
 
 
 def main():
-    pass
-
+    # Change this
+    handle = "MontellFish"
+    timeline = api.user_timeline(handle, count=50)
+    for tweet in timeline:
+        print("---- tweet ----\n")
+        print(tweet.text, "\n")
+    print(f"total tweets = {len(timeline)}")
 
 if __name__ == '__main__':
     main()
